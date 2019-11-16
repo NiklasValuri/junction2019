2f7929129d8c9ba9aa7455586f553949824eb8d4
Eero <eero.surakka@gmail.com>
2019-11-16 20:15:13 +0200

    stuff

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,49 +1,70 @@
 import React from 'react';
-import { Container, Header, Progress } from 'semantic-ui-react';
+import { Container, Header, Progress, Button } from 'semantic-ui-react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const InfoPage = () => {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push('/');
+  }
+
   return (
-    <Page>
-      <Title> Jumbo </Title>
+    <MainPage>
+      <Page>
+        <Flex>
+          <Title>Jumbo</Title>
+          <div>
+            <Button onClick={handleClick} primary>
+              Back
+            </Button>
+          </div>
+        </Flex>
 
-      <Gutter />
+        <Gutter />
 
-      <TextArea>
-        <Header as="h1">Site</Header>
+        <TextArea>
+          <Header as="h1">Purpose of the project</Header>
 
-        <Text>
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehe
-        </Text>
-      </TextArea>
+          <Text>
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehe
+          </Text>
+        </TextArea>
 
-      <TextArea>
-        <Header as="h1">Purpose of the project</Header>
+        <TextArea>
+          <Header as="h1">Site</Header>
 
-        <Text>
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehe
-        </Text>
-      </TextArea>
+          <Text>
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehe
+          </Text>
+        </TextArea>
 
-      <TextArea>
-        <Header as="h1">Project timeline</Header>
+        <TextArea>
+          <Header as="h1">Project timeline</Header>
 
-        <Text>Project started on 1.5.2019</Text>
+          <Text>Project started on 1.5.2019</Text>
 
-        <Text>
-          <Progress percent="55" indicating />
-        </Text>
-      </TextArea>
-    </Page>
+          <Text>
+            <Progress percent="75" indicating />
+          </Text>
+        </TextArea>
+      </Page>
+    </MainPage>
   );
 };
 
+const Flex = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+`;
+
 const Page = styled(Container)({
-  // backgroundColor: 'lightblue',
   paddingTop: '20px',
 });
 
@@ -61,9 +82,15 @@ const TextArea = styled.div`
   margin-bottom: 50px;
 `;
 
+const MainPage = styled.div`
+  height: 100vh;
+`;
+
 const Title = styled.p`
   font-size: 3em;
   font-weight: bold;
+  margin: 0;
+  padding: 0;
 `;
 
 export default InfoPage;
