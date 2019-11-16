14b88a81f8058b38fec0fb4de102362206a139ec
Eero <eero.surakka@gmail.com>
2019-11-16 17:50:06 +0200

    add infopage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import {
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
-import BuildingMap from './components/BuildingMap'
-import InfoPage from './components/InfoPage'
-import { Container } from 'semantic-ui-react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import BuildingMap from './components/BuildingMap';
+import InfoPage from './components/InfoPage';
 
-import Map from 'pigeon-maps'
-import Marker from 'pigeon-marker'
-import Overlay from 'pigeon-overlay'
+import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker';
+import Overlay from 'pigeon-overlay';
 
-const coords = [60.1654, 24.9374]
+const coords = [60.1654, 24.9374];
 
 const map = (
-  <Map 
-    center={coords} 
-    zoom={15} 
-    width={800} 
+  <Map
+    center={coords}
+    zoom={15}
+    width={800}
     height={500}
     provider={(x, y, z) => {
-      const s = String.fromCharCode(97 + (x + y + z) % 3)
-      return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
+      const s = String.fromCharCode(97 + ((x + y + z) % 3));
+      return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`;
     }}
     animate={true}
     mouseEvents={true}
@@ -30,23 +25,30 @@ const map = (
     <Marker anchor={coords} payload={1} onClick={({ event, anchor, payload }) => {}} />
 
     <Overlay anchor={coords} offset={[120, 79]}>
-      <img src='pigeon.jpg' width={240} height={158} alt='' />
+      <img src="pigeon.jpg" width={240} height={158} alt="" />
     </Overlay>
   </Map>
-)
+);
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/"  component={BuildingMap} />
-        <Route path="/info/:address" component={InfoPage} />
-        <Redirect to="/" />
-      </Switch>
-      Epic
-      {map}
-    </div>
-  )
+    <Switch>
+      <Route path="/info/:address" component={InfoPage} />
+
+      <Route path="/test">
+        <InfoPage />
+      </Route>
+
+      <Route path="/">
+        <div>
+          <BuildingMap />
+          {map}
+        </div>
+      </Route>
+
+      <Redirect to="/" />
+    </Switch>
+  );
 }
 
 export default App;
--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { Container, Header, Progress } from 'semantic-ui-react';
+import styled from 'styled-components';
 
-function InfoPage() {
+const InfoPage = () => {
   return (
-    <div>
-      Osoite 12 B
-    </div>
-  )
-}
+    <Page>
+      <Title> Jumbo </Title>
 
-export default InfoPage
+      <Gutter />
+
+      <TextArea>
+        <Header as="h1">Site</Header>
+
+        <Text>
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehe
+        </Text>
+      </TextArea>
+
+      <TextArea>
+        <Header as="h1">Purpose of the project</Header>
+
+        <Text>
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehe
+        </Text>
+      </TextArea>
+
+      <TextArea>
+        <Header as="h1">Project timeline</Header>
+
+        <Text>Project started on 1.5.2019</Text>
+
+        <Text>
+          <Progress percent="55" indicating />
+        </Text>
+      </TextArea>
+    </Page>
+  );
+};
+
+const Page = styled(Container)({
+  // backgroundColor: 'lightblue',
+  paddingTop: '20px',
+});
+
+const Text = styled.p`
+  font-size: 20px;
+  margin-left: 100px;
+`;
+
+const Gutter = styled.div`
+  height: 50px;
+  width: 50px;
+`;
+
+const TextArea = styled.div`
+  margin-bottom: 50px;
+`;
+
+const Title = styled.p`
+  font-size: 3em;
+  font-weight: bold;
+`;
+
+export default InfoPage;
