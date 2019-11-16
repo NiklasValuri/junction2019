0a4d8c2b298f5305a0572e76474a354025b63ae4
Kallemakela <kalle.makela@aalto.fi>
2019-11-16 12:24:45 +0200

    semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Epic
+      <Button>Click Here</Button>
     </div>
   );
 }
