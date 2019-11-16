4bfc700ab0eed4d4d6d559c3e4ae29cc0766f3d4
Kallemakela <kalle.makela@aalto.fi>
2019-11-17 00:22:52 +0200

    image fix v0

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,29 @@ const headerComponent = () => {
     return (
         <div style ={{
             background: "darkblue",
-            'padding-top': "10px",
-            'padding-left': '20px',
             height: "100px",
-            display: "flex"
+            display: "flex",
+            alignContent: 'center'
         }}> 
-            <img style = {{padding: "10px"}} src = "https://www.hel.fi/wps/wcm/connect/50249dfa-cfc1-46fe-84c5-25afaa8b9cab/stara_tunnus_Nega_verkkosivuille_3.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-50249dfa-cfc1-46fe-84c5-25afaa8b9cab-mIwVXes" />
-            <img style = {{ padding: "20px"}} src = "https://www.hel.fi/wps/CoHTheme/themes/html/RWD_main_theme_v2/img/HelsinkiUusiLogo.svg" />
+            <div style ={{
+                background: "darkblue",
+                height: "100px",
+                display: "flex",
+                alignContent: 'center'
+            }}>
+                <img style = {{padding: "20px 40px"}} src = "https://www.hel.fi/wps/wcm/connect/50249dfa-cfc1-46fe-84c5-25afaa8b9cab/stara_tunnus_Nega_verkkosivuille_3.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-50249dfa-cfc1-46fe-84c5-25afaa8b9cab-mIwVXes" />
+            </div>
+            <div style ={{
+                display: "flex",
+                alignContent: 'center',
+                padding: '0px',
+                margin: '0px',
+                marginLeft: 'auto'
+            }}>
+                <div>
+                    <img height='70' width='150' src = "https://www.hel.fi/wps/CoHTheme/themes/html/RWD_main_theme_v2/img/HelsinkiUusiLogo.svg" />
+                </div>
+            </div>
       </div>
     )
 }
