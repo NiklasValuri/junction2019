3608b19cbbe339b349ece12568f3aeb0b40c7e3f
Eero <eero.surakka@gmail.com>
2019-11-17 09:28:53 +0200

    fix header

--- src/components/header.js
+++ src/components/header.js
@@ -7,38 +7,22 @@ const headerComponent = () => {
         background: 'darkblue',
         height: '100px',
         display: 'flex',
-        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '0px 20px',
       }}
     >
-      <div
-        style={{
-          background: 'darkblue',
-          height: '100px',
-          display: 'flex',
-          alignContent: 'center',
-        }}
-      >
-        <img
-          style={{ padding: '20px 40px' }}
-          alt=""
-          src="https://www.hel.fi/wps/wcm/connect/50249dfa-cfc1-46fe-84c5-25afaa8b9cab/stara_tunnus_Nega_verkkosivuille_3.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-50249dfa-cfc1-46fe-84c5-25afaa8b9cab-mIwVXes"
-        />
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignContent: 'center',
-          padding: '0px',
-          margin: '0px',
-        }}
-      >
-        <div>
-          <img
-            alt=""
-            src="https://www.hel.fi/wps/CoHTheme/themes/html/RWD_main_theme_v2/img/HelsinkiUusiLogo.svg"
-          />
-        </div>
-      </div>
+      <img
+        height="50px"
+        alt=""
+        src="https://www.hel.fi/wps/wcm/connect/50249dfa-cfc1-46fe-84c5-25afaa8b9cab/stara_tunnus_Nega_verkkosivuille_3.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE-50249dfa-cfc1-46fe-84c5-25afaa8b9cab-mIwVXes"
+      />
+
+      <img
+        alt=""
+        height="50px"
+        src="https://www.hel.fi/wps/CoHTheme/themes/html/RWD_main_theme_v2/img/HelsinkiUusiLogo.svg"
+      />
     </div>
   );
 };
